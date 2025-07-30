65a7f203aefec428aca0302fb9690a6dfcba9a9d
Md Nafis Ul Haq Shifat <snafis@mit.edu>
2025-07-30 23:55:15 +0600

    Added tray so that it can run on bg

--- electron/clipboardManager.ts
+++ electron/clipboardManager.ts
@@ -15,33 +15,43 @@ export type ClipboardContent = {
 };
 
 // Stores the last seen HTML content to avoid duplicate entries.
-let lastHTML = getMostRecent().html;
-
+let lastClipboardHTML = getMostRecent().html;
 /**
- * Starts watching the system clipboard for changes.
+ * Starts watching the system clipboard for changes in background.
  * 
  * This function checks the clipboard every second. If new HTML content is detected
  * (i.e., different from the last seen HTML), it creates a new `ClipboardContent` object,
- * saves it to persistent history, and sends the new content to the renderer process.
+ * saves it to persistent history, and sends the new content to the renderer process (if multiple windows exist, it is sent to all of them).
  * 
- * @param window - The Electron `BrowserWindow` instance to which clipboard updates will be sent via IPC.
- * @returns A `NodeJS.Timeout` object representing the polling interval, which can be used to stop the watcher using `clearInterval`.
+ * @returns A `NodeJS.Timeout` object representing the polling interval
  */
-export function startClipBoardWatcher(window: BrowserWindow): NodeJS.Timeout {
-    const pollInterval = setInterval(() => {
+export function startBackgroundClipBoardWatcher(): NodeJS.Timeout {
+    return setInterval(() => {
         const currentHTML = clipboard.readHTML();
         const currentText = clipboard.readText();
-        if (currentHTML && currentHTML !== lastHTML) {
-            lastHTML = currentHTML;
+        if (currentHTML && currentHTML !== lastClipboardHTML) {
+            lastClipboardHTML = currentHTML;
             const content: ClipboardContent = {
                 text: currentText,
                 html: currentHTML,
                 timestamp: Date.now(),
             };
             saveClipboardContent(content);
-            window.webContents.send('clipboard-update', content);
+            const allWindows = BrowserWindow.getAllWindows();
+            allWindows.forEach(win => {
+                if (!win.isDestroyed() && win.webContents) {
+                    try {
+                        win.webContents.send('clipboard-update', {
+                            text: currentText,
+                            html: currentHTML,
+                            timestamp: Date.now()
+                        });
+                    } catch {
+                        console.log('No valid windows found, copied content saved in background.');
+                    }
+                }
+            });
         }
-    }, 1000); // every 1 second
-
-    return pollInterval;
+        
+    }, 1000); // Check every second
 }
